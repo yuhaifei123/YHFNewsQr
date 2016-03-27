9b5966ed35dc4b1327d9db01a2cfae32689d5f95
虞海飞 <1129639939@qq.com>
2016-03-27 15:25:01 +0800

    a

---
+++ NewsTableView/NewsTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsTableView
+//
+//  Created by 虞海飞 on 16/3/22.
+//  Copyright © 2016年 虞海飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
